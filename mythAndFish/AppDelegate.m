fc0ccf4c74a89b734ae8bb744da126ed588e3883
董良 <dongliang@haiwainet.cn>
2019-09-02 12:27:02 +0800

    111

--- mythAndFish/AppDelegate.m
+++ mythAndFish/AppDelegate.m
@@ -18,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    ///sssaaa
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     ViewController *rootVC = [[ViewController alloc] init];
